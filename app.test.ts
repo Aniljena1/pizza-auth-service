6442f94eb528ecfae6bd2eee13bd7603e73ce5e6
AnilJena1 <anil.jena@iserveu.in>
2023-12-07 18:12:13 +0530

    unit test

--- app.test.ts
+++ app.test.ts
@@ -1,8 +1,12 @@
 import request from "supertest";
 import app from "./src/app";
+import calculetDiscout from "./src/utils";
 
 describe("App", () => {
-   test("should work", () => {});
+   test("should work", () => {
+      const result = calculetDiscout(100, 10);
+      expect(result).toBe(10);
+   });
 
    test("should retuen 200", async () => {
       const response = await request(app).get("/").send();
---
+++ src/utils.ts
@@ -0,0 +1,5 @@
+const calculetDiscout = (price: number, percentage: number) => {
+   return price * (percentage / 100);
+};
+
+export default calculetDiscout;
