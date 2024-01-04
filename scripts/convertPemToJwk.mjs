8fce88b9a23e1cbf3e01f9ef6efde2ade6aaa276
AnilJena1 <anil.jena@iserveu.in>
2024-01-04 15:21:09 +0530

    add change file

--- scripts/convertPemToJwk.mjs
+++ scripts/convertPemToJwk.mjs
@@ -1,8 +1,7 @@
+import fs from "fs";
+import rsaPemToJwk from "rsa-pem-to-jwk";
 
-import fs from 'fs';
-import rsaPemToJwk from 'rsa-pem-to-jwk';
-
-const pravateKey = fs.readFileSync("./certs/private.pem")
+const pravateKey = fs.readFileSync("./certs/private.pem");
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-const jwk = rsaPemToJwk(pravateKey, { use: 'sig' }, "public")
-console.log(JSON.stringify(jwk))
+const jwk = rsaPemToJwk(pravateKey, { use: "sig" }, "public");
+console.log(JSON.stringify(jwk));
